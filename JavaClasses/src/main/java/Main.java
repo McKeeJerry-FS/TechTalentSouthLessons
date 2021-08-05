0fdc943a5c6d4add78f5634cff7b676b3fed8d42
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-08-04 20:18:40 -0400

    Testing the program as is.

--- JavaClasses/src/main/java/Main.java
+++ JavaClasses/src/main/java/Main.java
@@ -2,8 +2,13 @@ public class Main {
 
     public static void main(String[]Args) {
 
-//        Pet dog = new Pet("Perri", 1, "Home", "dog");
-//        dog.bark();
+        // Using the Pet Class
+        Pet dog = new Pet("Perri", 1, "Home", "dog");
+        dog.getName();
+        dog.setAge(2);
+        System.out.println("My dog is " + dog.getAge() + " years old.");
+
+        // Using the Aquarium Class
         Aquarium freshwater = new Aquarium("freshwater", 10, true);
         System.out.println(freshwater.isSetup(false));
     }
