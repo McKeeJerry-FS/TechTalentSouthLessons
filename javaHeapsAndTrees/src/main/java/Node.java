4db44d9012fd4c363d01bcdd43f8b64ff60dd583
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-08-16 11:43:24 -0400

    work in progress

---
+++ javaHeapsAndTrees/src/main/java/Node.java
@@ -0,0 +1,40 @@
+public class Node {
+
+    private int key;
+    private Node leftNode;
+    private Node rightNode;
+
+    public Node(int key, Node leftNode, Node rightNode) {
+        this.key = key;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+
+    public Node(int key) {
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public Node getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(Node rightNode) {
+        this.rightNode = rightNode;
+    }
+
+}
