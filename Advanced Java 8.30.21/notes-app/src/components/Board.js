ac864b77e2be47b26c48b17c213274ad5a3292b8
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-08-31 22:18:48 -0400

    update to Board.js

--- Advanced Java 8.30.21/notes-app/src/components/Board.js
+++ Advanced Java 8.30.21/notes-app/src/components/Board.js
@@ -1,29 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../css/Board.css';
-import Note from '../components/Note';
+import Note from './Note';
 
 class Board extends Component {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+        this.state = {
+            notes: [
+                {
+                    title: "Class Notes",
+                    body: "Always use constructors when extending another class",
+                },
+                {
+                    title: "Bloop",
+                    body: "blop",
+                },
+                {
+                    title: "Third title",
+                    body: "Third body",
+                },
+            ]
+        }
+    }
 
-  render() {
-    return (
-      <div>
-        <div className="div-board">
-          <div className="row">
-            <Note title="Class Notes" body="Always use constructors when extending another class "/>
-            <Note title="Bloop" body="bloop"/>
-            <Note title="Third Note" body="third note"/>
-            <Note />
-          </div>
-        </div>
-        <div>
-          <button className="btn btn-success add-button">Add</button>
-        </div>
-      </div>
-    );
-  }
+    addNote() {
+        let notes = this.state.notes;
+        notes.push(
+            {
+                title: "New Title",
+                body: "New Body",
+            }
+        );
+        this.setState({notes});
+
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <div className="div-board">
+                    <div className="row">
+
+                        {
+                         this.state.notes.map(note => <Note title={note.title} body={note.body} />)
+                        }
+          
+                    </div>
+                </div>
+                <div>
+                    <button onClick={this.addNote.bind(this)} className="btn btn-success add-button">
+                        Add
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Board;
