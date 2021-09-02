d1ec47080f2984dbf78ee2944907ba5e678c7be2
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-02 09:34:26 -0400

    built out the navbar

--- Advanced Java 8.30.21/react-router-restaurant/src/index.js
+++ Advanced Java 8.30.21/react-router-restaurant/src/index.js
@@ -56,6 +56,18 @@ class Menu extends Component {
   }
 }
 
+// Creating a Navbar
+class Navbar extends Component {
+  render(){
+    return (
+      <div className="nav">
+        <Link to="/">Home</Link>  
+        <Link to="menu">Menu</Link>  
+        <Link to="about">About</Link>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
